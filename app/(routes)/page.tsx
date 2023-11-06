d22e468a4da3c8eb1fa561ce6b8471350ae733b3
Vivek Kumar <vkhp77@gmail.com>
2023-11-06 14:44:49 +0530

    First

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const products = await getProducts({isFeatured: true});
+    const billboard = await getBillboard("47e84d9c-e764-4654-97d9-3ee33a643e50");
+
+    return ( 
+        <Container>
+            <div>
+                <Billboard data={billboard}/>
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products} />
+            </div>
+        </Container>
+     );
+}
+ 
+export default HomePage;
